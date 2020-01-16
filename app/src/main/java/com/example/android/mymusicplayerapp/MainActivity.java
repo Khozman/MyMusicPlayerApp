007e79d26e664a386f3c4eaf31cc9bf6499f8c61
Khozman <bhekinkosikhosa@gmail.com>
2020-01-16 11:22:52 +0200

    Creating the access storage media permission

--- app/src/main/java/com/example/android/mymusicplayerapp/MainActivity.java
+++ app/src/main/java/com/example/android/mymusicplayerapp/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.android.mymusicplayerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    This is the permissions String array which is linked to the manifest page, to enable the app to read extenal storage...
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    private static final int REQUEST_PERMSSIONS = 12345;
+
+    private static final int PERMISSIONS_COUNT = 1;
+
+    @SuppressLint("NewApi")
+    private boolean arePermissionsDenied(){
+        for(int i = 0; i < PERMISSIONS_COUNT; i++){
+            if(checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(arePermissionsDenied()){
+            ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+            recreate();
+        } else {
+            onResume();
+        }
+    }
+
+    private boolean isMusicPlayerInit;
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionsDenied()){
+            requestPermissions(PERMISSIONS, REQUEST_PERMSSIONS);
+            return;
+        }
+
+        if(!isMusicPlayerInit){
+            final ListView listView = findViewById(R.id.listView);
+
+            isMusicPlayerInit = true;
+        }
+
+    }
+
 }
