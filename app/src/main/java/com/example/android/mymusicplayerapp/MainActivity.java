a54a5d8996038c5e7d4b012ad019d015b8820fb6
Khozman <bhekinkosikhosa@gmail.com>
2020-01-17 06:09:10 +0200

    adding the method of enabling mp3 format search and the start song method

--- app/src/main/java/com/example/android/mymusicplayerapp/MainActivity.java
+++ app/src/main/java/com/example/android/mymusicplayerapp/MainActivity.java
@@ -15,7 +15,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.File;
@@ -86,8 +88,72 @@ public class MainActivity extends AppCompatActivity {
         addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)));
     }
 
-    private void playMusicFile(String path){
-        MediaPlayer mp = new MediaPlayer();
+//    Making variable mp to be a global variable so that it makes it easy to access in other methods...
+
+    private MediaPlayer mp;
+    private int songPosition;
+    private volatile boolean isSongPlaying;
+    private TextView songPositionTextView;
+    private TextView songDurationTextView;
+    private SeekBar seekBar;
+    private View playBackControls;
+    private Button pauseButton;
+    private int mPosition;
+
+    private void playSong(){
+        final String musicFilePath = musicFilesList.get(mPosition);
+        final int songDuration = playMusicFile(musicFilePath)/1000;
+        seekBar.setMax(songDuration);
+        seekBar.setVisibility(View.VISIBLE);
+        playBackControls.setVisibility(View.VISIBLE);
+
+//                  Converting the songDuration of time from milliseconds to mm:ss...
+        songDurationTextView.setText(String.valueOf(songDuration/60)+":"+String.valueOf(songDuration%60));
+
+        new Thread() {
+            public void run() {
+
+                songPosition = 0;
+                isSongPlaying = true;
+                while (songPosition < songDuration) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (isSongPlaying) {
+                        songPosition++;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                seekBar.setProgress(songPosition);
+
+                                //Converting the songDuration of time from milliseconds to mm:ss...
+                                songPositionTextView.setText(String.valueOf(songPosition / 60) + ":" + String.valueOf(songPosition % 60));
+                            }
+                        });
+                    }
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mp.pause();
+                        songPosition = 0;
+                        mp.seekTo(songPosition);
+                        songPositionTextView.setText("0");
+                        pauseButton.setText("play");
+                        isSongPlaying = false;
+                        seekBar.setProgress(songPosition);
+                    }
+                });
+            }
+        }.start();
+    }
+
+
+    private int playMusicFile(String path){
+        mp = new MediaPlayer();
 
         try{
             mp.setDataSource(path);
@@ -96,7 +162,12 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e){
             e.printStackTrace();
         }
+        return mp.getDuration();
     }
+
+
+
+
 
     @Override
     protected void onResume(){
@@ -114,11 +185,62 @@ public class MainActivity extends AppCompatActivity {
             fillMusicList();
             textAdapter.setData(musicFilesList);
             listView.setAdapter(textAdapter);
+
+//            adding the seekBar..
+            seekBar = findViewById(R.id.seekBar);
+
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+//              Enabling the seekBar to control the selected track...
+
+                int songProgress;
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    songProgress = progress;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    songProgress = songPosition;
+                    mp.seekTo(songProgress);
+                }
+            });
+
+            songPositionTextView = findViewById(R.id.currentPosition);
+            songDurationTextView = findViewById(R.id.songDuration);
+            pauseButton = findViewById(R.id.pause);
+            playBackControls = findViewById(R.id.playBackButtons);
+
+            pauseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                 if(isSongPlaying) {
+                     mp.pause();
+                     pauseButton.setText("play");
+                 } else {
+                     if (songPosition == 0) {
+                         playSong();
+                     } else {
+                         mp.start();
+                         pauseButton.setText("pause");
+                     }
+                 }
+                 isSongPlaying = !isSongPlaying;
+                }
+            });
+
+
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    final String musicFilePath = musicFilesList.get(position);
-                    playMusicFile(musicFilePath);
+                    mPosition = position;
+                    playSong();
                 }
             });
 
